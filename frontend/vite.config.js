d034da0e5288f15a2eaea04bd102aa41646e7bab
zhangweimin <zhangweimin@wps.cn>
2026-03-28 19:17:03 +0800

    feat: 将打包文件直接重定向到django后端

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import path from 'path'
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,5 +17,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  // 打包配置重定向到Django static，这样就可以在Django中访问静态文件
+  // npm run build 后，会打包到 backend/static/frontend 目录下
+  build: {
+    outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static
+    emptyOutDir: true,
   },
 })
