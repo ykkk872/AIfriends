65b845d3a829c9193f0107a3cd869fb43033d4e2
zhangweimin <zhangweimin@wps.cn>
2026-03-29 21:57:39 +0800

    feat: 实现注册，登陆页面

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -41,7 +41,22 @@ async function handleLogin() {
 
 <template>
   <div class="flex justify-center mt-30">
-    登录页面
+    <form @submit.prevent="handleLogin" class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <label class="label">用户名</label>
+      <input v-model="username" type="text" class="input" placeholder="用户名" />
+
+      <label class="label">密码</label>
+      <input v-model="password" type="password" class="input" placeholder="密码" />
+
+      <p v-if="errorMessage" class="text-sm text-red-500 mt-1">{{ errorMessage }}</p>
+
+      <button class="btn btn-neutral mt-4">登录</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'user-account-register-index'}" class="btn btn-sm btn-ghost text-gray-500">
+          注册
+        </RouterLink>
+      </div>
+    </form>
   </div>
 </template>
 
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -43,7 +43,27 @@ async function handleRegister() {
 </script>
 
 <template>
-  
+  <div class="flex justify-center mt-30">
+    <form @submit.prevent="handleRegister" class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <label class="label">用户名</label>
+      <input v-model="username" type="text" class="input" placeholder="用户名" />
+
+      <label class="label">密码</label>
+      <input v-model="password" type="password" class="input" placeholder="密码" />
+
+      <label class="label">确认密码</label>
+      <input v-model="passwordConfirmed" type="password" class="input" placeholder="确认密码" />
+
+      <p v-if="errorMessage" class="text-sm text-red-500 mt-1">{{ errorMessage }}</p>
+
+      <button class="btn btn-neutral mt-4">注册</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'user-account-login-index'}" class="btn btn-sm btn-ghost text-gray-500">
+          登录
+        </RouterLink>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
